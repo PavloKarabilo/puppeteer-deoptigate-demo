b9a9a701b9b4cf296de33ae8a66e73ced02b1a0b
PavloKarabilo <pavlo.karabilo@dazn.com>
2021-01-21 13:59:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+const inquirer = require('inquirer');
+const {exec} = require('child_process');
+
+
+const runDeoptigate = () => {
+    exec('./node_modules/.bin/deoptigate', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        process.exit(0);
+    });
+}
+
+(async () => {
+  let res = await inquirer.prompt([{type: 'input', name: 'url', message: 'Please enter URL'}]);
+  const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+      args: [
+          '--disable-extensions',
+          '--incognito',
+          '--no-sandbox',
+          '--js-flags="--trace-deopt --redirect-code-traces=true --log-all --no-logfile-per-isolate --logfile=' + __dirname + 'v8.log',
+      ]
+  });
+
+  console.log(res.url);
+  const [page] = await browser.pages();
+  await page.goto(res.url);
+
+  page.on('close', runDeoptigate);
+  browser.on('disconnected', runDeoptigate);
+
+  process.on('SIGINT', () => {
+        runDeoptigate();
+        process.exit();
+  });
+  
+})();
